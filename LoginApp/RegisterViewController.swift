5a0e8570f642ad4bf23fef770c92f32e18bd3116
puditr <puditr@gmail.com>
2019-02-21 13:23:46 +0700

    Check Space and Alert Success

--- LoginApp/RegisterViewController.swift
+++ LoginApp/RegisterViewController.swift
@@ -10,21 +10,53 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
+    } // Main Method
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func uploadButton(_ sender: UIBarButtonItem) {
+        let name : String = nameTextField.text!
+        let user : String = userTextField.text!
+        let password : String = passwordTextField.text!
+        
+        print("name ==> \(name) user ==> \(user) password ==>\(password)")
+        
+        if (name.count == 0) || (user.count == 0) || (password.count == 0) {
+            // Have Space
+            registerAlert(title: "Have Space", message: "Please Fill All Blanks")
+        } else {
+            
+        } // If else
+        
+        
+        
+        
+        
+        
+    } // Upload Button
+    
+    func registerAlert(title : String , message : String) -> Void {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+            
+        }))
+        
+        present(alert, animated: true, completion: nil)
+        
     }
-    */
-
-}
+    
+} // Main Class
