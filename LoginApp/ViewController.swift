b56032875dd5a7b7a2627a9018b60d34b218b5ae
puditr <puditr@gmail.com>
2019-02-21 10:18:31 +0700

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Pudit Rungsettee on 21/2/2562 BE.
+//  Copyright © 2562 Pudit Rungsettee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
