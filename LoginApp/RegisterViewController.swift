0fd6df1906c89e418fa4e1913574ea071a3aae4b
puditr <puditr@gmail.com>
2019-02-21 11:50:52 +0700

    Icon Success

---
+++ LoginApp/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  LoginApp
+//
+//  Created by Pudit Rungsettee on 21/2/2562 BE.
+//  Copyright © 2562 Pudit Rungsettee. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
